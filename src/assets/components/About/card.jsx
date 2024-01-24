e357ee635b16e9bae4346331792b8e1a75160a38
FernandaG <fernanda.guemez@gmail.com>
2024-01-24 17:29:51 -0600

    Se agregan iconos

--- src/assets/components/About/card.jsx
+++ src/assets/components/About/card.jsx
@@ -6,9 +6,12 @@ import CardMedia from "@mui/material/CardMedia";
 import Button from "@mui/material/Button";
 import Typography from "@mui/material/Typography";
 import cardImage from "../About/fer.jpg";
+import GitHubIcon from "@mui/icons-material/GitHub";
+import LinkedInIcon from "@mui/icons-material/LinkedIn";
+import InstagramIcon from "@mui/icons-material/Instagram";
 
 export default function AboutCard() {
-  const { names, profile, city, state, email } = {
+  const { names, profile, city, email } = {
     names: "Maria Fernanda Guemez Medina",
     profile: "Desarrolladora Java Full Stack Jr.",
     city: "Mérida, Yucatán",
@@ -16,7 +19,11 @@ export default function AboutCard() {
   };
   return (
     <Card className="card" sx={{ maxWidth: 400 }}>
-      <CardMedia sx={{ height: 400 }} image={cardImage} title="green iguana" />
+      <CardMedia
+        sx={{ height: 400 }}
+        image={cardImage}
+        title="fernanda sonriendo"
+      />
       <CardContent>
         <Typography gutterBottom variant="h4" component="div">
           {names}
@@ -47,14 +54,21 @@ export default function AboutCard() {
           target="_blank"
           href="https://github.com/FernandaGuemez"
         >
-          GitHub
+          <GitHubIcon sx={{ fontSize: 50 }}></GitHubIcon>
         </Button>
         <Button
           size="small"
           target="_blank"
           href="https://www.linkedin.com/in/maria-fernanda-g%C3%BC%C3%A9mez-medina-568140291/"
         >
-          LinkedIn
+          <LinkedInIcon sx={{ fontSize: 50 }}></LinkedInIcon>
+        </Button>
+        <Button
+          size="small"
+          target="_blank"
+          href="https://www.instagram.com/ferguemez_/?hl=es-la"
+        >
+          <InstagramIcon sx={{ fontSize: 50 }}></InstagramIcon>
         </Button>
       </CardActions>
     </Card>
